26a8d243f49a1542f179d4fc6df4143cd2e6475d
Afif Ahmed <afif.ahmed@yahoo.com>
2021-11-10 18:18:16 +0600

    Basic Data insert in the database

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-const hello = 'hello world!';
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Hi Node js active for this project")
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a3ykp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("bestTreadmillHome");
+        const products = database.collection("products");
+        const reviews = database.collection("reviews");
+        // create a document to insert
+        const doc = {
+            title: "Afif Ahmed",
+            content: "You are NIce",
+        }
+        // const result = await reviews.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port, () => {
+    console.log("Node Listening From port:", port)
+})
